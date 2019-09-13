29c5bdb6aee3ee5b81cf7bc04a3c578d06c326c1
Orlando Del Aguila <orlando@hashlabs.com>
2019-09-12 19:04:14 -0500

    feat: save and destroy jwt cookie on login and logout

--- plugin.rb
+++ plugin.rb
@@ -1,94 +1,152 @@
+# frozen_string_literal: true
+
 # name: discourse-debtcollective-sso
 # about: Extensions to Discourse SSO provider to work the way we need
 # version: 0.0.2
 # authors: @debtcollective
 
+require 'jwt'
+
 after_initialize do
-  # This is needed by the dc-vue-header in order to work
-  class Discourse::Cors
-    def self.apply_headers(cors_origins, env, headers)
-      origin = nil
+  class DebtCollectiveSSO
+    COOKIE_DOMAIN = ENV["JWT_COOKIE_DOMAIN"] || ".lvh.me"
+    COOKIE_NAME = 'tdc_auth_token'
+    JWT_SECRET = ENV['JWT_SECRET'] || 'testing123'
 
-      if cors_origins
-        if origin = env['HTTP_ORIGIN']
-          origin = nil unless cors_origins.include?(origin)
-        end
+    def initialize(user, cookies = {})
+      @user = user
+      @cookies = cookies
+    end
 
-        headers['Access-Control-Allow-Origin'] = origin || cors_origins[0]
-        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-CSRF-Token, Discourse-Visible'
-        headers['Access-Control-Expose-Headers'] = 'X-Discourse-Username'
-        headers['Access-Control-Allow-Credentials'] = 'true'
-        headers['Access-Control-Allow-Methods'] = 'HEAD, OPTIONS, GET, DELETE'
+    def generate_jwt
+      hmac_secret = JWT_SECRET
+      jwt_alg = "HS256"
+
+      token = JWT.encode(jwt_payload, hmac_secret, jwt_alg)
+    end
+
+    def set_jwt_cookie
+      domain = ENV["JWT_COOKIE_DOMAIN"] || ".lvh.me"
+      secure = Rails.env.production?
+
+      @cookies[COOKIE_NAME] = {
+        domain: COOKIE_DOMAIN,
+        expires: SiteSetting.maximum_session_age.hours.from_now,
+        httponly: true,
+        secure: SiteSetting.force_https,
+        value: generate_jwt,
+      }
+    end
+
+    def remove_jwt_cookie
+      @cookies.delete(COOKIE_NAME, domain: COOKIE_DOMAIN)
+    end
+
+    private
+
+    def user_avatar_url
+      avatar_url = @user.small_avatar_url
+
+      if @user.uploaded_avatar.present?
+        base_url = Discourse.store.external? ? "#{Discourse.store.absolute_base_url}/" : Discourse.base_url
+        avatar_url = "#{base_url}#{Discourse.store.get_path_for_upload(@user.uploaded_avatar)}"
       end
 
-      headers
+      avatar_url
+    end
+
+    def user_profile_background_url
+      if @user.user_profile.profile_background_upload.present?
+        profile_background_url = UrlHelper.absolute(upload_cdn_path(
+          @user.user_profile.profile_background_upload.url
+        ))
+      end
+    end
+
+    def user_card_background_url
+      if @user.user_profile.card_background_upload.present?
+        card_background_url = UrlHelper.absolute(upload_cdn_path(
+          @user.user_profile.card_background_upload.url
+        ))
+      end
+    end
+
+    def user_custom_fields
+      custom_fields = {
+        zip_code: @user.custom_fields.fetch("user_field_1", "").to_s,
+        state: @user.custom_fields.fetch("user_field_2", "").to_s,
+        phone_number: @user.custom_fields.fetch("user_field_3", "").to_s
+      }
+    end
+
+    def jwt_payload
+      groups = @user.groups.collect(&:name)
+
+      payload = {
+        active: @user.active,
+        admin: @user.admin?,
+        avatar_url: user_avatar_url,
+        card_background_url: user_card_background_url,
+        created_at: @user.created_at,
+        custom_fields: user_custom_fields,
+        email: @user.email,
+        external_id: @user.id,
+        groups: groups,
+        last_seen_at: @user.last_seen_at,
+        moderator: @user.moderator?,
+        name: @user.name,
+        profile_background_url: user_profile_background_url,
+        updated_at: @user.updated_at,
+        username: @user.username,
+      }
     end
   end
 
-  # SSO payload to return whitelisted user custom_fields
-  module DebtCollectiveSessionController
-    def sso_provider(payload = nil)
-      payload ||= request.query_string
+  class DebtCollectiveCurrentUserProvider < ::Auth::DefaultCurrentUserProvider
+    def log_on_user(user, session, cookies, opts = {})
+      super(user, session, cookies, opts)
 
-      if SiteSetting.enable_sso_provider
-        sso = SingleSignOnProvider.parse(payload)
+      DebtCollectiveSSO.new(user, cookies).set_jwt_cookie
+    end
 
-        if sso.return_sso_url.blank?
-          render plain: "return_sso_url is blank, it must be provided", status: 400
-          return
+    def log_off_user(session, cookies)
+      super(session, cookies)
+
+      DebtCollectiveSSO.new(current_user, cookies).remove_jwt_cookie
+    end
+
+    def refresh_session(user, session, cookies)
+      # if user was not loaded, no point refreshing session
+      # it could be an anonymous path, this would add cost
+      return if is_api? || !@env.key?(CURRENT_USER_KEY)
+
+      if !is_user_api? && @user_token && @user_token.user == user
+        rotated_at = @user_token.rotated_at
+
+        needs_rotation = @user_token.auth_token_seen ? rotated_at < UserAuthToken::ROTATE_TIME.ago : rotated_at < UserAuthToken::URGENT_ROTATE_TIME.ago
+
+        if needs_rotation
+          if @user_token.rotate!(user_agent: @env['HTTP_USER_AGENT'],
+                                 client_ip: @request.ip,
+                                 path: @env['REQUEST_PATH'])
+            cookies[TOKEN_COOKIE] = cookie_hash(@user_token.unhashed_auth_token)
+
+            # extend to set jwt cookie when refreshing session
+            DebtCollectiveSSO.new(user, cookies).set_jwt_cookie
+          end
         end
+      end
 
-        if current_user
-          sso.name = current_user.name
-          sso.username = current_user.username
-          sso.email = current_user.email
-          sso.external_id = current_user.id.to_s
-          sso.admin = current_user.admin?
-          sso.moderator = current_user.moderator?
-          sso.groups = current_user.groups.pluck(:name).join(",")
+      if !user && cookies.key?(TOKEN_COOKIE)
+        cookies.delete(TOKEN_COOKIE)
 
-          if current_user.uploaded_avatar.present?
-            base_url = Discourse.store.external? ? "#{Discourse.store.absolute_base_url}/" : Discourse.base_url
-            avatar_url = "#{base_url}#{Discourse.store.get_path_for_upload(current_user.uploaded_avatar)}"
-            sso.avatar_url = UrlHelper.absolute Discourse.store.cdn_url(avatar_url)
-          else
-            # return letter_avatar if no uploaded_avatar
-            sso.avatar_url = current_user.small_avatar_url
-          end
-
-          if current_user.user_profile.profile_background_upload.present?
-            sso.profile_background_url = UrlHelper.absolute(upload_cdn_path(
-              current_user.user_profile.profile_background_upload.url
-            ))
-          end
-
-          if current_user.user_profile.card_background_upload.present?
-            sso.card_background_url = UrlHelper.absolute(upload_cdn_path(
-              current_user.user_profile.card_background_upload.url
-            ))
-          end
-
-          # return user fields
-          sso.custom_fields["user_state"] = current_user.custom_fields.fetch("user_field_1", "").to_s
-          sso.custom_fields["user_zip"] = current_user.custom_fields.fetch("user_field_2", "").to_s
-          sso.custom_fields["user_phone_number"] = current_user.custom_fields.fetch("user_field_3", "").to_s
-
-          if request.xhr?
-            cookies[:sso_destination_url] = sso.to_url(sso.return_sso_url)
-          else
-            redirect_to sso.to_url(sso.return_sso_url)
-          end
-        else
-          cookies[:sso_payload] = request.query_string
-          redirect_to path('/login')
-        end
-      else
-        render body: nil, status: 404
+        # extend to remove jwt cookie
+        DebtCollectiveSSO.new(current_user, cookies).remove_jwt_cookie
       end
     end
   end
 
-  ::SessionController.class_eval do
-    prepend DebtCollectiveSessionController
+  if SiteSetting.enable_debtcollective_sso
+    Discourse.current_user_provider = DebtCollectiveCurrentUserProvider
   end
 end

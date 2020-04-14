36e9c736e1a721162a141fcab4f0c59517abbf40
Orlando Del Aguila <orlando@hashlabs.com>
2020-04-14 17:16:22 -0500

    fix(custom-wizard): fix custom wizard redirection after signup
    custom wizard redirection check method has a side effect we need to run before redirecting to other
    apps (ex. strike website). Wit this fix, the next time the user visits Discourse, it will be
    redirected to the wizard

--- plugin.rb
+++ plugin.rb
@@ -118,13 +118,18 @@ after_initialize do
           @user.enqueue_welcome_message('welcome_user') if @user.send_welcome_message
           log_on_user(@user)
 
+          # CustomWizard plugin has side effect when calling Wizard.user_requires_completion?(@user)
+          # We still need this side effect so the wizard is rendered when the user goes to Discourse for the first time
+          custom_wizard_redirect = Wizard.user_requires_completion?(@user)
+
           # Redirect to SSO signup before Wizards
           if sso_destination_url = cookies[:sso_destination_url]
-            cookies[:sso_destination_url] = nil
+            cookies.delete(:sso_destination_url)
+
             return redirect_to(sso_destination_url)
           end
 
-          if Wizard.user_requires_completion?(@user)
+          if custom_wizard_redirect
             return redirect_to(wizard_path)
           elsif destination_url = cookies[:destination_url]
             cookies[:destination_url] = nil

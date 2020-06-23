4801406f5e8c91763841c51cd8f79a6bdbfa481e
Orlando Del Aguila <orlando@hashlabs.com>
2020-06-23 12:11:16 -0500

    dev(Algolia): add REST AlgoliaPlaces client

--- lib/algolia_places_client.rb
+++ lib/algolia_places_client.rb
@@ -4,35 +4,53 @@ require 'uri'
 
 module Debtcollective
   class AlgoliaPlacesClient
-    attr_reader :app_id, :api_key, :api_url
-
-    def initialize(app_id: nil, api_key: nil)
-      return nil unless app_id.present? && api_key.present?
-
-      self.app_id = app_id
-      self.api_key = api_key
-      self.api_url = 'https://places-dsn.algolia.net/1/places/query'
-    end
-
-    # Returns HTTP::Response
-    def query(query, options = { type: "address", "restrictSearchableAttributes": "postcode", hitsPerPage: 1 })
+    # Returns JSON response or nil
+    def self.query(query, options = { type: "address", "restrictSearchableAttributes": "postcode", hitsPerPage: 1 })
       payload = { query: query }.merge(options)
+      api_url = 'https://places-dsn.algolia.net/1/places/query'
 
-      Net::HTTP.post(
-        URI(self.api_url),
+      response = Net::HTTP.post(
+        URI(api_url),
         payload.to_json,
         self.headers
       )
+
+      case response
+      when Net::HTTPSuccess then
+        success_response(response)
+      else
+        # TODO: track error with sentry?
+        nil
+      end
     end
 
     private
 
-    def headers
+    def self.success_response(response)
+      body = response.body
+      json = JSON.parse(body)
+
+      # We return the first result
+      result = json['hits'].first
+
+      {
+        city: result['city']['default'].first,
+        country: result['country']['default'],
+        country_code: result['country_code'],
+        county: result['country']['default'].first,
+        objectID: result['objectID'],
+        postcodes: result['postcode'],
+        state: result['administrative'].first
+      }
+
+    end
+
+    def self.headers
       {
         accept: "application/json",
         "Content-Type": "application/json",
-        "X-Algolia-Application-Id": self.app_id,
-        "X-Algolia-API-Key": self.api_key
+        "X-Algolia-Application-Id": SiteSetting.debtcollective_algolia_app_id,
+        "X-Algolia-API-Key": SiteSetting.debtcollective_algolia_api_key
       }
     end
   end

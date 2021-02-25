74abd25dbed27d9ff6f574fde60fae2190df4e24
Orlando Del Aguila <orlando@hashlabs.com>
2021-02-24 19:53:33 -0600

    feat(email-token): add u/email-token endpoint
    this is an admin only endpoint

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe "UsersController" do
-  describe 'Create user via api' do
+  describe '#create' do
     context "with an admin account" do
       it 'returns user data including auto generated username' do
         post_user_params ={
@@ -42,6 +42,58 @@ describe "UsersController" do
         expect(response.status).to eq(400)
         expect(json['errors']).to be_present
         expect(json['success']).to eq(nil)
+      end
+    end
+  end
+
+  describe '#email_token' do
+    context "with an admin account" do
+      it 'returns email_token if user was found' do
+        user = Fabricate(:user)
+        api_key = Fabricate(:api_key, user: Fabricate(:admin))
+        params = {
+          login: user.email
+        }
+
+        post "/u/email-token", params: params, headers: { HTTP_API_KEY: api_key.key }
+        json = response.parsed_body
+
+        user.reload
+
+        expect(response.status).to eq(200)
+        expect(json['success']).to eq("OK")
+        expect(json['user_found']).to eq(true)
+        expect(json['email_token']).to eq(user.email_tokens.last.token)
+      end
+
+      it 'returns user_found false if no user is found' do
+        user = Fabricate(:user)
+        api_key = Fabricate(:api_key, user: Fabricate(:admin))
+        params = {
+          login: "test@example.com"
+        }
+
+        post "/u/email-token", params: params, headers: { HTTP_API_KEY: api_key.key }
+        json = response.parsed_body
+
+        expect(response.status).to eq(200)
+        expect(json['success']).to eq("OK")
+        expect(json['user_found']).to eq(false)
+        expect(json['email_token']).not_to be_present
+      end
+    end
+
+    context "with a user account" do
+      it 'returns not found' do
+        user = Fabricate(:user)
+        api_key = Fabricate(:api_key, user: Fabricate(:user))
+        params = {
+          login: user.email
+        }
+
+        post "/u/email-token", params: params, headers: { HTTP_API_KEY: api_key.key }
+
+        expect(response.status).to eq(404)
       end
     end
   end
